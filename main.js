4cc2c12782fa8a5d185471037e30da3baf5d39b6
Willem van Heemstra <wvanheemstra@icloud.com>
2016-03-03 20:59:27 +0000

    Modified

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 import React from '../local_libraries/react';
 import Router from '../local_libraries/react-router';
 import ReactDOM from '../local_libraries/react-dom';
-import createBrowserHistory from '../local_libraries/history/lib/createBrowserHistory';
+import createBrowserHistory from '../local_libraries/createBrowserHistory';
 import routes from '../local_routes/creation_ecosystem_route';
 import Navbar from '../local_components/Navbar';
 
