29b6688dc3e5d93eabd0c220e4fa5c2df0607a8b
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-03-16 14:34:30 +0000

    Modified

--- application.js
+++ application.js
@@ -3,12 +3,16 @@
  * 
  */
 module.exports = function() {
-	var _Application = {};
-	config = require('../configurations/server.js');
-	var configs = config.configs,
-		server_prefix = configs.server_prefix || 'PREFIX';
-	var express = require('express');
-	console.log(server_prefix + " - Applications application required.");
-	_Application = express();
-	return _Application;
+  var _Application = {};
+  var path = require('../libraries/path');
+  var paths = require('../paths/paths');
+  //OLD config = require('../configurations/server.js');
+  config = require(path.join(paths.configurations, '/configuration.js'))
+  var configs = config.configs,
+  server_prefix = configs.server_prefix || 'PREFIX';
+  //OLD var express = require('express');
+  var express = require(path.join(paths.libraries, '/express.js'))
+  console.log(server_prefix + " - Applications application required.");
+  _Application = express();
+  return _Application;
 }();//calls itself
