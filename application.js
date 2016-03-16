974245816e201a5dacdc74fadeb6144d8aab1a26
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-03-16 15:21:15 +0000

    Modified

--- application.js
+++ application.js
@@ -6,11 +6,10 @@ module.exports = function() {
   var _Application = {};
   var path = require('../libraries/path');
   var paths = require('../paths/paths');
-  //OLD config = require('../configurations/server.js');
-  config = require(path.join(paths.configurations, '/configuration.js'))
-  var configs = config.configs,
-  server_prefix = configs.server_prefix || 'PREFIX';
-  //OLD var express = require('express');
+  var app = _Application;
+  config = require(path.join(paths.configurations, '/configurations.js'))(app);
+  var common = config.common,
+  server_prefix = common.server_prefix || 'PREFIX';
   var express = require(path.join(paths.libraries, '/express.js'))
   console.log(server_prefix + " - Applications application required.");
   _Application = express();
